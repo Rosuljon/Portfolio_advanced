e9828c8467affce2ac0c5ca2e87a73c541276629
Ronny <ikramjanov97@mail.ru>
2022-12-22 15:13:35 +0900

    change structure on importing components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import NavBar from "../components/NavBar";
-import Main from "../components/Main";
-import About from "../components/About";
-import Skills from "../components/Skills";
+import {NavBar, Main, About, Skills} from "../components"
 export default function Home() {
   return (
     <div>
