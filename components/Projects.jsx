93bab8bdebf524e9ff72185261bc6429c0027fc5
Ronny <ikramjanov97@mail.ru>
2023-01-10 18:35:09 +0900

    finished project section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import Image  from 'next/image';
-import wallet from '../public/assets/projects/wallet.png'
-import exp from '../public/assets/projects/exp.png'
+import ProjectItem from './ProjectItem';
+import wallet from '../public/assets/projects/wallet.png';
+import explorer from '../public/assets/projects/explorer.png';
+import troy from '../public/assets/projects/Troya.png';
+import quote from '../public/assets/projects/12.png';
+import meetup from '../public/assets/projects/meet-up.png';
+import crypto from '../public/assets/projects/crypto.png';
+
 
 const Projects = () => {
+    const data =[
+        {name: "Wallet for BlockChain Tech.", framework: "Svelte js", photo: wallet, link: "https://wallet.niktonet.com"},
+        {name: "Explorer for BlockChain Tech.", framework: "Svelte js", photo: explorer, link: "https://explorer.niktonet.com"},
+        {name: "Troy Kebab Food App", framework: "React js", photo: troy, link: "https://troykebab.netlify.app"},
+        {name: "Best Quote App", framework: "React js", photo: quote, link: "https://best-quote.netlify.app"},
+        {name: "Meet-up", framework: "Svelte js", photo: meetup, link: "https://meet-up-ronny.netlify.app"},
+        {name: "Crypto App", framework: "React js", photo: crypto, link: "https://crypto-ronny.netlify.app"},
+    ];
   return (
     <div className='w-full'>
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className="uppercase text-xl tracking-widest text-[#5651e5]">Projects</p>
             <h2 className="py-2">What i've built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
-                <div className='relative flex justify-center items-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-                <Image src={exp} alt='12'></Image>
-                </div>
-                <div className='relative flex justify-center items-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-                    <Image src={wallet} alt='/'></Image>
-                </div>
+                {data.map((project) => (
+                    <ProjectItem key={project.name} project={project} />
+                ))}
             </div>
         </div>
     </div>
