8f3b6874ae9fb4a4aaa4042c9ddf5427ba70657f
Ronny <ikramjanov97@mail.ru>
2023-01-17 16:37:57 +0900

    Fixed a bug

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import AboutImg from "../public/assets/about.png"
+import AboutImg from "../public/assets/about.png";
+import Link from 'next/link';
 const About = () => {
   return (
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
@@ -26,9 +27,9 @@ const About = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <a href="/#projects">
+          <Link href="/#projects">
           <p className="py-2 text-gray-600 underline cursor-pointer dark:text-white">Check out some of my latest projects</p>
-          </a>
+          </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
         <Image src={AboutImg} className='rounded-xl md:min-h-[200px]' alt='/' />
