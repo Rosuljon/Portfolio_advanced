93d2f3fd608845eb2f2b3dc1d66a0947128f7ef2
Rosuljon <ikramjanov97@mail.ru>
2023-01-26 20:41:53 +0900

    fix1

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 
 const Main = () => {
   return (
-    
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
