c22a22c468a3e64ce322f9e97084aaee1755c8b4
Ronny <ikramjanov97@mail.ru>
2023-04-14 17:06:54 +0900

    og image added

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,20 @@ export default function Home() {
     <div>
       <Head>
         <title>Ronny Dev | MERN Stack Developer</title>
-        <meta name="description" content="Ronny Dev's Portfolio website" />
-        <meta property="og:image"  content="/assets/transparent.png"></meta>
+        {/* <meta name="description" content="Ronny Dev's Portfolio website" />
+        <meta property="og:image" content="/assets/transparent.png"></meta> */}
+        <meta property="og:title" content="Ronny Dev | MERN Stack Developer" />
+        <meta property="og:site_name" content="ronny7.netlify.app" />
+        <meta property="og:description" content="Ronny Dev's Portfolio website" />
+        <meta property="og:image" content="http://www.ronny7.netlify.app/assets/transparent.png" />
         <link rel="icon" href="/assets/r_dev.png" />
       </Head>
       <div className="dark:bg-gradient-to-r from-[#4568dc] to-[#b06ab3] dark:text-white">
-      <Main />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+        <Main />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
       </div>
     </div>
   );
