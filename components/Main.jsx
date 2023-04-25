fcf873ec42f1eef90080274842bee2c236033279
Ronny <ikramjanov97@mail.ru>
2023-04-25 09:59:29 +0900

    fix: issue

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,7 +9,7 @@ import Ronny from "../public/assets/graduate.png";
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center pt-10">
         <div>
           <div className="flex justify-center my-8 pt-10">
           <Image
